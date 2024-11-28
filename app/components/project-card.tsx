dfd64a6a50bee1f5759ef2a1b477f5d3e499bbfc
Baran <phscloq@gmail.com>
2024-11-28 23:50:02 +0300

    Update ProjectCard component to change images from <Image> tag to <img> to avoid using next/image

--- app/components/project-card.tsx
+++ app/components/project-card.tsx
@@ -5,16 +5,19 @@ type ProjectCardProps = {
     title: string;
     demo: string;
     github: string;
+    alt:string;
+    big: boolean;
     }
 
-export default function ProjectCard({img, title, demo, github}: ProjectCardProps){
+export default function ProjectCard({img, title, demo, github, alt,big}: ProjectCardProps){
     return (
-       
-        <div className=' relative bg-slate-500  min-h-64  sm:min-h-72 w-96 sm:min-w-full rounded-2xl overflow-hidden   shadow-lg  shadow-slate-900 flex'>
+       <>
+        {
+          big ? (<div className=' relative bg-slate-500  min-h-64  sm:min-h-72 w-96 sm:min-w-full rounded-2xl overflow-hidden   shadow-lg  shadow-slate-900 flex'>
             
             <div className='absolute top-0 left-0 w-full '>
-              <Image className=' min-w-[384px] w-full min-h-64 sm:min-h-72' 
-              src={img} alt="Women Page Screenshot" 
+              <img className=' min-w-[384px] w-full min-h-64 sm:min-h-72' 
+              src={img} alt={alt} 
               width={384} height={221} />
             </div>
 
@@ -27,10 +30,38 @@ export default function ProjectCard({img, title, demo, github}: ProjectCardProps
 
               <Link href={github}
                   target='_blank'>
-                  <Image className='' src="/icons/github.svg" alt="GitHub" width={48} height={48} />
+                  <img className='' src="/icons/github.svg" alt="GitHub" width={48} height={48} />
+              </Link>
+            </div>
+        </div>)
+      :(
+        <div className=' relative bg-slate-500 
+        rounded-2xl overflow-hidden    min-h-[126px] min-w-[220px]
+        shadow-lg  shadow-slate-900 flex'>
+            
+            <div className='absolute top-0 left-0 w-full h-full'>
+              <img className='  object-contain h-full w-full ' 
+              src={img} alt={alt} 
+              />
+            </div>
+
+            <div className='flex items-end px-4 pb-1  justify-between z-10 w-full'>
+              <Link href={demo} className="hover:underline text-black text-sm" target='_blank'>
+                <p className=' font-extrabold relative'>{title}
+                <span className="absolute -top-1 -right-4 -rotate-45 text-sm ">{'->'}</span>
+                </p>
+              </Link>
+
+              <Link href={github}
+                  target='_blank'>
+                  <img className='' src="/icons/github.svg" alt="GitHub" width={28} height={28} />
               </Link>
             </div>
         </div>
+      )
+      }
+          </>
+
     )
 }
 
