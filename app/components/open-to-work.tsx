a09c51f4fac536296789161ba1b27e6702ca691d
Baran <phscloq@gmail.com>
2024-11-28 23:50:58 +0300

    Add OpenToWork component to state current employment status

---
+++ app/components/open-to-work.tsx
@@ -0,0 +1,7 @@
+export default function (){
+    return (
+        <div className="bg-gray-400 absolute rounded-full left-1/2 -translate-x-1/2 -translate-y-1/2 transform transition-transform ease-in-out animation-duration-2s animation-timing-function-ease-in-out">
+  <p className="text-red-700 font-semibold text-2xl animate-spin">Open to work!</p>
+</div>
+    )
+}
