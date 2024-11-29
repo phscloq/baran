bcc4c52084a0d148d2f4312a427c9940ca4ba378
Baran <phscloq@gmail.com>
2024-11-29 23:58:11 +0300

    Add MiniProjects component to display a list of mini projects

---
+++ app/components/mini-projects.tsx
@@ -0,0 +1,39 @@
+export default function MiniProjects(){
+
+    const projects = [
+        {
+            title: "Guess the Color!",
+            alt:"Guess the Color! An HEX color guessing game",
+            img: "/images/habits-warrior.png",
+            demo: "https://habitswarrior.com/",
+            technologies: ["NextJs", "React", "TypeScript", "TailwindCSS", "Supabase", "Vercel", "Git", "GitHub"],
+        },
+        {
+            title: "Search Flight",
+            alt:"A flight search web app based on mock data",
+            img:"/images/shopping-cart-women-page.png",
+            demo:"https://shopping-site-rho.vercel.app/",
+            github:"https://github.com/phscloq/shopping-site",
+            technologies: ["NextJs", "React", "TypeScript", "TailwindCSS", "Supabase", "Vercel", "Git", "GitHub"],
+        },
+        {
+            title: "Election Observer",
+            alt:"A mobile app to help observersin elections",
+            img:"/images/ui-page.png",
+            demo:"https://ui-library-seven.vercel.app/",
+            github:"https://github.com/phscloq/ui-library",
+            technologies: ["NextJs", "React", "TypeScript", "TailwindCSS", "Supabase", "Vercel", "Git", "GitHub"],
+        },
+        {
+            title: "CSS Challanges",
+            alt:"A repo of the CSS challanges I've done",
+            img:"/images/ui-page.png",
+            demo:"https://ui-library-seven.vercel.app/",
+            github:"https://github.com/phscloq/ui-library",
+            technologies: ["NextJs", "React", "TypeScript", "TailwindCSS", "Supabase", "Vercel", "Git", "GitHub"],
+        },
+
+    ]
+
+
+}
