bacae75527059257850ddfeb8817f1898a24ff9f
Baran <phscloq@gmail.com>
2024-11-30 00:00:03 +0300

    Refactor Skills component to dynamically render tech stack

--- app/components/skills.tsx
+++ app/components/skills.tsx
@@ -1,20 +1,22 @@
 export default function Skills(){
+    const techStack = [
+        { category: "Frontend", technologies: ["Typescript", "React", "Next.js"] },
+        { category: "Backend", technologies: ["Node.js", "Express"] },
+        { category: "Tools", technologies: ["Git"] },
+    ]
+
     return (
-        <div 
-        className='
-        relative bg-slate-500  min-h-48 w-96 sm:min-w-full 
-        rounded-2xl overflow-hidden px-8 py-4 '>
-        <ul className="flex flex-wrap gap-4 text-slate-100 ">
-            <li>Typescript</li>
-            <li>React</li>
-            <li>Next.js</li>
-            <li>Node.js</li>
-            <li>Express</li>
-            <li>MongoDB</li>
-            <li>Supabase</li>
-            <li>Tailwindcss</li>
-            <li>Git</li>
-        </ul>
+        <div>
+            {techStack.map(({ category, technologies }) => (
+                <div key={category}>
+                    <h1>{category}</h1>
+                    {technologies.map((title)=>{
+                        return(
+                            <h3 key={title}>{title}</h3>
+                        )
+                    })}
+                </div>
+            ))}
         </div>
 
     )
