1c72dd2883a6733a4f0b37d6f215ae9a7e4e2113
Baran <phscloq@gmail.com>
2024-11-29 23:57:21 +0300

    Add Footer component to display contact information

---
+++ app/components/footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer(){
+    return (
+        <footer className='hidden h-36  gap-2 sm:flex justify-center items-center
+            '>
+              <p className='text-slate-700  font-extrabold '>Email: </p>
+              <p className='text-slate-700 font-bold select-all'>y.barandgn@gmail.com</p>
+      </footer> 
+    )
+}
