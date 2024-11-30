3bb4c8592b56fed8127fbd066709a67d803cf410
Baran <phscloq@gmail.com>
2024-11-30 11:55:05 +0300

    Update MainProjects component to include project descriptions and tech stack passing as props to ProjectCard

--- app/components/main-projects.tsx
+++ app/components/main-projects.tsx
@@ -6,14 +6,15 @@ export default function MainProjects(){
         {
             title: "Habits Warrior",
             alt:"Habits Warrior: A habit tracking app",
+            description: "A habit tracking app that helps you build good habits and break bad ones.",
             img:"/images/ui-page.png",
             demo: "https://habitswarrior.com/",
-            github:"https://github.com/phscloq/ui-library",
             technologies: ["NextJs", "React", "TypeScript", "TailwindCSS", "Supabase", "Vercel", "Git", "GitHub"],
         },
         {
             title: "Baran UI",
             alt:"UI page screenshot",
+            description: "A collection of reusable UI components for rapid web development.",
             img:"/images/ui-page.png",
             demo:"https://ui-library-seven.vercel.app/",
             github:"https://github.com/phscloq/ui-library",
@@ -22,6 +23,7 @@ export default function MainProjects(){
         {
             title: "Shopping Cart",
             alt:"Shopping-cart website image",
+            description: "An e-commerce platform showcasing advanced shopping cart functionality. Demonstrates proficiency in state management and creating intuitive user interfaces.",
             img:"/images/shopping-cart-women-page.png",
             demo:"https://shopping-site-rho.vercel.app/",
             github:"https://github.com/phscloq/shopping-site",
@@ -31,8 +33,8 @@ export default function MainProjects(){
     ]
 
     return (
-        <section id="main-projects">
-            <h2 className="text-2xl font-bold">Featured Projects</h2>
+        <section id="main-projects" className="mt-16">
+            <h2 className="text-2xl font-bold mb-8">Featured Projects</h2>
             <div className="grid grid-cols-1 md:grid-cols-2  gap-4">
                 {projects.map((project)=>(
                     <ProjectCard
@@ -43,6 +45,8 @@ export default function MainProjects(){
                     github={project.github}
                     alt={project.alt}
                     key={project.title}
+                    techStack={project.technologies}
+                    description={project.description}
                     />
                 ))}
             </div>
