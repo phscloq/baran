cc3b8c49cee5719192ab4c2c22a78650ffd589f9
Baran <phscloq@gmail.com>
2024-11-30 12:00:03 +0300

    Refactor Skills component to improve structure and styling

--- app/components/skills.tsx
+++ app/components/skills.tsx
@@ -1,23 +1,29 @@
+import Badge from "./badge"
+
 export default function Skills(){
     const techStack = [
-        { category: "Frontend", technologies: ["Typescript", "React", "Next.js"] },
-        { category: "Backend", technologies: ["Node.js", "Express"] },
-        { category: "Tools", technologies: ["Git"] },
+        { category: "Frontend", technologies: ["React", "Next.js", "TypeScript", "JavaScript", "Tailwind CSS", "HTML5", "CSS3", "Vite"] },
+        { category: "Backend", technologies: ["Node.js", "Express", "Supabase", "MongoDB", "Firebase"] },
+        { category: "Tools", technologies: ["Git", "GitHub", "Figma", "Responsive Design"] },
     ]
 
     return (
-        <div>
-            {techStack.map(({ category, technologies }) => (
-                <div key={category}>
-                    <h1>{category}</h1>
-                    {technologies.map((title)=>{
-                        return(
-                            <h3 key={title}>{title}</h3>
-                        )
-                    })}
-                </div>
-            ))}
-        </div>
-
+        <section id="skills">
+            <h2 className="text-2xl font-bold mb-8">Skills</h2>
+            <div className=" space-y-4">
+                {techStack.map(({ category, technologies }) => (
+                    <div key={category} className="mb-4">
+                        <h1>{category}</h1>
+                        <div className="flex gap-2 mt-2">
+                        {technologies.map((title)=>{
+                            return(
+                                <Badge key={title}>{title}</Badge>
+                            )
+                        })}
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </section>
     )
 }
