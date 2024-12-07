23c1178d35f1bb5606e4bbaf46be6a48ff89d332
Baran <phscloq@gmail.com>
2024-12-07 23:23:47 +0300

    Update slug
    Changed the folder name to slug. Imported projectsData and updated the code according to the data.

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,24 +1,15 @@
 'use client'
 import { useEffect, useState } from "react"
-import MainProjects from "../components/main-projects"
-import { mainProjects } from "../data/projects"
+import { projectsData } from "../data/projects"
 import Container from "../components/container"
 import Navbar from "../components/navbar"
 import Badge from "../components/badge"
 import Link from "next/link"
-export default function ProjectPage({params}:{params:{projectTitle:string}}){
+export default function ProjectPage({params}:{params:{slug:string}}){
     const [project, setProject] = useState<any>(null)
     useEffect(()=>{
-        const originalTitle = params.projectTitle.replace(/-/g, ' ')
-        const project = mainProjects.find(
-            (project) => project.title.toLowerCase() === originalTitle.toLowerCase()
-        );
-        setProject(project)
-        console.log("Project", project)
-        console.log("Params", params.projectTitle)
-        console.log("MainProjects", MainProjects)
-
-    },[params.projectTitle])
+      setProject(projectsData[params.slug as keyof typeof projectsData])
+    },[params.slug])
 
     if(project){
     
@@ -31,40 +22,66 @@ export default function ProjectPage({params}:{params:{projectTitle:string}}){
             <div className='bg-white dark:bg-slate-700 p-8 rounded-[2.5rem] shadow-md '>
                 <h1 className="text-3xl font-bold mb-4 text-blue-600 dark:text-blue-400">{project.title}</h1>
                 <div className="mb-8">
-                    <img src={project.img} alt={project.title} className="w-full h-64 object-cover rounded-lg" />
+                    <img src={project.image} alt={project.title} className="w-full  object-fill rounded-lg" />
                 </div>
 
                 <div className="mb-6">
                     <h2 className="text-xl font-semibold mb-2">Project Overview</h2>
-                    <p className="text-gray-600 dark:text-gray-300">{project.description}</p>
+                    <p className="text-gray-600 dark:text-gray-300">{project.fullDescription}</p>
                 </div>
 
                 <div className="mb-6">
                     <h2 className="text-xl font-semibold mb-2">Tech Stack</h2>
                     <div className="flex flex-wrap gap-2">
-                    {project.technologies.map((tech:string)=>(
+                    {project.techStack.map((tech:string)=>(
                         <Badge key={tech}>{tech}</Badge>
                     ))}
                     </div>
                 </div>
-                <div className="flex justify-between mt-8">
-                {project.demo && 
-                <Link href={project.demo}>
-                <button className="bg-slate-200 text-black px-3 py-1 rounded-md 
-                hover:bg-slate-300  active:bg-slate-500 active:text-slate-100
-                ">
-                  Live
-                </button>
-              </Link>}
+                
+                <div className="mb-6">
+                    <h2 className="text-xl font-semibold mb-2">Features</h2>
+                    <ul className="list-disc list-inside text-gray-600 dark:text-gray-300">
+                        {project.features.map((feature:string)=>(
+                            <li key={feature}>{feature}</li>
+                        ))}
+                    </ul>
+                </div>
 
-              {project.github && 
-              <Link href={project.github}>
-                <button className="bg-slate-200 text-black px-3 py-1 rounded-md 
-                hover:bg-slate-300  active:bg-slate-500 active:text-slate-100
-                ">
-                  GitHub
-                </button>
-              </Link>}
+                <div className="mb-6">
+                  <h2 className="text-xl font-semibold mb-2">Challenges</h2>
+                    <p>{project.challenges}</p>
+                </div>
+                
+                <div className="mb-6">
+                  <h2 className="text-xl font-semibold mb-2">Future Improvements</h2>
+                  <p>{project.futureImprovements}</p>
+                </div> 
+
+                {/* Model for future use
+                <div className="mb-6">
+                  <h2 className="text-xl font-semibold mb-2">Section Title</h2>
+                </div> */}
+
+                {/* Links for demo and github */}
+                <div className="flex justify-between mt-8">
+                  {project.liveUrl && 
+                  <Link href={project.liveUrl}>
+                    <button className="bg-slate-200 text-black px-3 py-1 rounded-md 
+                    hover:bg-slate-300  active:bg-slate-500 active:text-slate-100
+                    ">
+                      Live
+                    </button>
+                  </Link>}
+
+                  {project.githubUrl && 
+                  <Link href={project.githubUrl}>
+                    <button className="bg-slate-200 text-black px-3 py-1 rounded-md 
+                    hover:bg-slate-300  active:bg-slate-500 active:text-slate-100
+                    ">
+                      GitHub
+                    </button>
+                  </Link>}
                 </div>
             </div>
         </section>
