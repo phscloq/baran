c13ad16cc4bc6fd79c7d59cc118520f883cab7f9
Baran <phscloq@gmail.com>
2024-12-18 23:57:22 +0300

    Update main-projects and mini-projects components
    - Updated the code to use `projectsData`.
    - Changed the props based on the `projectsData`.

--- app/components/main-projects.tsx
+++ app/components/main-projects.tsx
@@ -1,51 +1,24 @@
 import ProjectCard from "./project-card"
-
+import { projectsData } from "../data/projects"
 export default function MainProjects(){
 
-    const projects = [
-        {
-            title: "Habits Warrior",
-            alt:"Habits Warrior: A habit tracking app",
-            description: "A habit tracking app that helps you build good habits and break bad ones.",
-            img:"/images/ui-page.png",
-            demo: "https://habitswarrior.com/",
-            technologies: ["NextJs", "React", "TypeScript", "TailwindCSS", "Supabase", "Vercel", "Git", "GitHub"],
-        },
-        {
-            title: "Baran UI",
-            alt:"UI page screenshot",
-            description: "A collection of reusable UI components for rapid web development.",
-            img:"/images/ui-page.png",
-            demo:"https://ui.ybarandogan.com",
-            github:"https://github.com/phscloq/ui-library",
-            technologies: ["NextJs", "React", "TypeScript", "TailwindCSS", "Supabase", "Vercel", "Git", "GitHub"],
-        },
-        {
-            title: "Shopping Cart",
-            alt:"Shopping-cart website image",
-            description: "An e-commerce platform showcasing advanced shopping cart functionality. Demonstrates proficiency in state management and creating intuitive user interfaces.",
-            img:"/images/shopping-cart-women-page.png",
-            demo:"https://shopping-site-rho.vercel.app/",
-            github:"https://github.com/phscloq/shopping-site",
-            technologies: ["NextJs", "React", "TypeScript", "TailwindCSS", "Supabase", "Vercel", "Git", "GitHub"],
-        },
-       
-    ]
+    const projects = Object.values(projectsData).slice(0, 3)
 
     return (
         <section id="main-projects" className="mt-16">
             <h2 className="text-2xl font-bold mb-8">Featured Projects</h2>
-            <div className="grid grid-cols-1 md:grid-cols-2  gap-4">
+            <div className="grid grid-cols-1 gap-4">
                 {projects.map((project)=>(
                     <ProjectCard
                     big={false}
-                    img={project.img}
+                    img={project.image}
+                    darkThemedImage={project.darkThemedImage}
                     title={project.title}
-                    demo={project.demo}
-                    github={project.github}
-                    alt={project.alt}
+                    demo={project.liveUrl}
+                    github={project.githubUrl}
+                    alt={project.description}
                     key={project.title}
-                    techStack={project.technologies}
+                    techStack={project.techStack}
                     description={project.description}
                     />
                 ))}
--- app/components/mini-projects.tsx
+++ app/components/mini-projects.tsx
@@ -1,45 +1,8 @@
 import ProjectCard from "./project-card"
-
+import { projectsData } from "../data/projects"
 export default function MiniProjects(){
+    const projects = Object.values(projectsData).slice(3, )
 
-    const projects = [
-        {
-            title: "Guess the Color!",
-            description: "An HEX color guessing game. Test your knowledge of HEX colors!",
-            alt:"Guess the Color! An HEX color guessing game",
-            img: "/images/guess-the-color.png",
-            demo: "https://phscloq.github.io/guess-the-color/",
-            github:"https://github.com/phscloq/guess-the-color",
-            technologies: ["NextJs", "React", "TypeScript", "TailwindCSS", "Supabase", "Vercel", "Git", "GitHub"],
-        },
-        {
-            title: "Search Flight",
-            description: "Interactive flight search application with advanced filtering and sorting capabilities.",
-            alt:"A flight search web app based on mock data",
-            img:"/images/shopping-cart-women-page.png",
-            demo:"https://flight-search-app-bay.vercel.app",
-            github:"https://github.com/phscloq/flight-search-app",
-            technologies: ["NextJs", "React", "TypeScript", "TailwindCSS", "Supabase", "Vercel", "Git", "GitHub"],
-        },
-        {
-            title: "Election Observer",
-            description: "Mobile app for election observers to document and report election processes.",
-            alt:"A mobile app to help observersin elections",
-            img:"/images/ui-page.png",
-            github:"https://github.com/phscloq/secim-app",
-            technologies: ["NextJs", "React", "TypeScript", "TailwindCSS", "Supabase", "Vercel", "Git", "GitHub"],
-        },
-        {
-            title: "CSS Challenges",
-            description: "Collection of advanced CSS implementations showcasing frontend expertise.",
-            alt:"A repo of the CSS challenges I've done",
-            img:"/images/ui-page.png",
-            demo:"https://challenges-inky.vercel.app",
-            github:"https://github.com/phscloq/challenges",
-            technologies: ["NextJs", "React", "TypeScript", "TailwindCSS", "Supabase", "Vercel", "Git", "GitHub"],
-        },
-
-    ]
     return (
         <section id="mini-projects" className="mt-16">
             <h2 className="text-2xl font-bold mb-8">Other Projects</h2>
@@ -47,13 +10,14 @@ export default function MiniProjects(){
                 {projects.map((project)=>(
                     <ProjectCard
                     big={true}
-                    img={project.img}
+                    img={project.image}
+                    darkThemedImage={project.darkThemedImage}
                     title={project.title}
-                    demo={project.demo}
-                    github={project.github}
-                    alt={project.alt}
+                    demo={project.liveUrl}
+                    github={project.githubUrl}
+                    alt={project.description}
                     key={project.title}
-                    techStack={project.technologies}
+                    techStack={project.techStack}
                     description={project.description}
                     />
                 ))}
