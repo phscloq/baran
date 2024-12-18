f14e777f9cb696bce6b35da885c5f35bb58a0ef5
Baran <phscloq@gmail.com>
2024-12-18 23:00:34 +0300

    Update Home page
    Added Summary section.

--- app/page.tsx
+++ app/page.tsx
@@ -7,15 +7,18 @@ import ProfileInfo from './components/profile'
 import MainProjects from './components/main-projects'
 import MiniProjects from './components/mini-projects'
 import Education from './components/education'
+import Summary from './components/summary'
 //import OpenToWork from './components/open-to-work'
 export default function Home() {
   return (
-<main className="  min-h-screen bg-gradient-to-b  from-gray-50 to-gray-100 dark:from-slate-900 dark:to-slate-800  text-slate-800 dark:text-white">
+<main className="  min-h-screen bg-gradient-to-b  from-gray-50 to-gray-100
+ dark:from-slate-900 dark:to-slate-800  text-slate-800 dark:text-white">
   <Container>
     <Navbar />
     <section className='py-8'>
       <div className='bg-white dark:bg-slate-700 p-8 rounded-[2.5rem] shadow-md '>
         <ProfileInfo />
+        <Summary />
         <Skills />
         <Education />
         <MainProjects />
