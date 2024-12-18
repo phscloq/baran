4ff40421f4500e3bb49f40531c96bdbf2f781d10
Baran <phscloq@gmail.com>
2024-12-18 22:59:00 +0300

    Create summary.tsx

---
+++ app/components/summary.tsx
@@ -0,0 +1,13 @@
+export default function Summary(){
+    return (
+        <section id="summary" className="mb-4">
+            <h2 className="text-2xl font-bold ">Summary</h2>
+            <div 
+                className="bg-slate-100 dark:bg-slate-600 py-4 px-3 rounded-md">           
+                <p className="">
+                    I am a frontend developer with a passion for creating beautiful and functional user interfaces. I have experience with React, Next.js, TypeScript, JavaScript and Tailwind CSS. I am always looking for new opportunities to learn and grow as a developer.
+                </p>
+            </div>
+        </section>
+    )
+}
