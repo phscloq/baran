b8e02f97b7c3ce1f2eaaa37e906eba15fbb0a839
Baran <phscloq@gmail.com>
2025-01-27 00:56:33 +0300

    Simplify theme toggle logic and remove mounting check
    - Removed unnecessary `useEffect` and `useState` for mounting check.
    
    - Simplified the rendering logic by using `hidden` and `block` classes for conditional icon display.
    
    - Improved code readability and reduced complexity.

--- app/components/light-toggle.tsx
+++ app/components/light-toggle.tsx
@@ -2,25 +2,9 @@
 
 import { useTheme } from "@/app/light-context";
 import { Sun, Moon } from "lucide-react";
-import { useEffect, useState } from "react";
 export default function LightToggle(){
     const {theme, toggleTheme} = useTheme();
-    const [isMounted, setIsMounted] = useState(false);
-
-    useEffect(() => {
-        setIsMounted(true); // Set mounted to true after component mounts
-    }, []);
     
-    if (!isMounted) {
-        return (
-            <button 
-        className=" bg-white text-black dark:hover:text-slate-100 hover:bg-slate-100 dark:hover:bg-slate-800  dark:bg-slate-700 dark:text-white 
-        border  rounded-md  p-2 text-4xl  dark:border-0  h-[42px] w-[42px]
-        transition-colors duration-300 ease-in-out"
-        onClick={() => toggleTheme()}
-        ></button>
-        )
-      }
 
     return (
         <button 
@@ -29,8 +13,8 @@ export default function LightToggle(){
         transition-colors duration-300 ease-in-out"
         onClick={() => toggleTheme()}
         >
-            {theme === 'dark' ? <Sun className="h-6 w-6" />
-            : <Moon className="h-6 w-6"/>}
+            <Sun className="h-6 w-6 hidden dark:block" />
+            <Moon className="h-6 w-6 block dark:hidden" />
             
             
         </button>
