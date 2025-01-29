c151b99bf3e6e75c36c358a15756199a156e6f89
Baran <phscloq@gmail.com>
2025-01-29 23:23:07 +0300

    Update metadata for SEO and social sharing
    Updated metadata with title, description, and keywords.

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,52 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: "Baran's Portfolio",
-  description: 'Personal portfolio website',
+  description: 'Welcome to my personal portfolio website! Explore my projects, skills, and experience in web development and design.',
+  keywords: [
+    'Baran',
+    'Portfolio',
+    'Web Development',
+    'Frontend Developer',
+    'React',
+    'Next.js',
+    'JavaScript',
+    'TypeScript',
+    'Tailwind CSS',
+    'UI/UX Design',
+  ],
+  authors: [{ name: 'Baran', url: 'https://www.ybarandogan.com' }],
+  openGraph: {
+    title: "Baran's Portfolio",
+    description: 'Welcome to my personal portfolio website! Explore my projects, skills, and experience in web development and design.',
+    url: 'https://www.ybarandogan.com',
+    siteName: "Baran's Portfolio",
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: "Baran's Portfolio",
+    description: 'Welcome to my personal portfolio website! Explore my projects, skills, and experience in web development and design.',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: false,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  icons: {
+    icon: '/favicon.ico', // Replace with your favicon path
+    shortcut: '/favicon-16x16.png', // Replace with your shortcut icon path
+    apple: '/apple-touch-icon.png', // Replace with your Apple touch icon path
+  },
+  manifest: '/site.webmanifest', 
 }
 
 export default function RootLayout({
